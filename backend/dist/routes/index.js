12e9bafdd27b55b847f8f55bc94a036f55145fd7
hauntingofsam <dattashamoyita@gmai.com>
2024-02-29 13:51:11 +0530

    api routes removed

--- backend/dist/routes/index.js
+++ backend/dist/routes/index.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import userRoutes from "./user-routes.js";
 import chatRoutes from "./chat-routes.js";
 const appRouter = Router();
-appRouter.use("/api/user", userRoutes);
-appRouter.use("/api/chat", chatRoutes);
+appRouter.use("/user", userRoutes);
+appRouter.use("/chat", chatRoutes);
 export default appRouter;
 //# sourceMappingURL=index.js.map
