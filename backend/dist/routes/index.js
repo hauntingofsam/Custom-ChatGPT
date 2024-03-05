436d02788c36d2175387509e3b2b1c7226e6bf07
nazmus-saqib008 <iamnazmussaqib@outlook.com>
2024-03-05 20:57:55 +0530

    Revert "api routes removed"
    This reverts commit 12e9bafdd27b55b847f8f55bc94a036f55145fd7.

--- backend/dist/routes/index.js
+++ backend/dist/routes/index.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import userRoutes from "./user-routes.js";
 import chatRoutes from "./chat-routes.js";
 const appRouter = Router();
-appRouter.use("/user", userRoutes);
-appRouter.use("/chat", chatRoutes);
+appRouter.use("/api/user", userRoutes);
+appRouter.use("/api/chat", chatRoutes);
 export default appRouter;
 //# sourceMappingURL=index.js.map
